74fd50b2be6a9bc186f6deb14f6b8bf12bcd0d07
Markus Penttila <markus.penttila@solita.fi>
2014-05-16 14:29:55 +0300

    Add custom icon for bus stops.

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,39 @@
 var app = angular.module("HervantaApp", ["leaflet-directive"]);
 
-app.controller("MapCtrl", [ "$scope", "$http", function($scope, $http) {
-  angular.extend($scope, {
-    defaults: {
-        tileLayer: "https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png",
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        id: 'examples.map-i86knfo3'
-    },
-    tampere: {
-        lat: 61.50,
-        lng: 23.76,
-        zoom: 13
-    }
-  });
-  $scope.markers = [];
-    $http.get('/stations')
-    .success(function(stations){
-        stations = stations.map(function(station){
-            return {lat: station.y, lng: station.x, message: station.name};
-        });
-        $scope.markers = stations;
+app.controller("MapCtrl", [ "$scope", "$http", function ($scope, $http) {
+    angular.extend($scope, {
+        defaults: {
+            tileLayer: "https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png",
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            id: 'examples.map-i86knfo3'
+        },
+        tampere: {
+            lat: 61.50,
+            lng: 23.76,
+            zoom: 13
+        }
     });
-  $scope.nextLines = [];
-  for (var i = 0; i < 15; i++) {
-    $scope.nextLines.push({name: i});
-  }
+    var icon = {
+        iconUrl: '/public/images/bus_stop.png',
+        iconSize: [20, 20],
+        shadowSize: [0, 0],
+        iconAnchor: [10, 10], // point of the icon which will correspond to marker's location
+        popupAnchor: [0, 0]
+    };
+    $scope.markers = [];
+    $http.get('/stations')
+        .success(function (stations) {
+            stations = stations.map(function (station) {
+                return {lat: station.y, lng: station.x, message: station.name, icon: icon};
+            });
+            $scope.markers = stations;
+        });
+    $scope.nextLines = [];
+    for (var i = 0; i < 15; i++) {
+        $scope.nextLines.push({name: i});
+    }
+
 }]);
